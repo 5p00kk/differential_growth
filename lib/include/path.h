72c41617a9fdb6087667abb56d73720792b1de1d
5p00kk <szymon.kowalewski@gmail.com>
2022-05-15 13:29:24 +0200

    clean up comments

--- lib/include/path.h
+++ lib/include/path.h
@@ -17,12 +17,8 @@ public:
     void add_node(std::shared_ptr<c_node> node);
     void print_path() const;
     void apply_brownian();
+    /* KD-tree required functions */
     inline size_t kdtree_get_point_count() const { return m_nodes.size(); }
-
-    // Returns the dim'th component of the idx'th point in the class:
-    // Since this is inlined and the "dim" argument is typically an immediate
-    // value, the
-    //  "if/else's" are actually solved at compile time.
     inline double kdtree_get_pt(const size_t idx, const size_t dim) const
     {
         if (dim == 0)
