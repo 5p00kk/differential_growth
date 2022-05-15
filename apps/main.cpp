3a55f9462df134c72daa0ec1e7a903772bcf89bd
5p00kk <szymon.kowalewski@gmail.com>
2022-05-15 14:17:23 +0200

    display things

--- apps/main.cpp
+++ apps/main.cpp
@@ -2,6 +2,7 @@
 #include "node.h"
 #include "path.h"
 #include "visualizer.h"
+#include <opencv2/highgui/highgui.hpp>
 
 int main(int argc, char const *argv[])
 {
@@ -11,24 +12,26 @@ int main(int argc, char const *argv[])
     dg::c_visualizer visualizer;
 
     printf("Creating ptr\n");
-    for(int i=0;i<100;i++)
+    for(int i=0;i<300;i++)
     {
         printf("Created ptr\n");
-        path.add_node(std::make_shared<dg::c_node>(i, i));
+        path.add_node(std::make_shared<dg::c_node>(i+300, i+300));
     }
     printf("added %ld nodes\n", path.kdtree_get_point_count());
     path.print_path();
 
-    
-    path.apply_brownian();
-    printf("Applied brownian\n");
-    path.update_path();
-    printf("Updated path\n");
-    path.print_path();
-    printf("The end\n");
 
     cv::Mat output_image;
-    visualizer.visualize(path.m_first_node, output_image);
+    for(int step=0; step<1000; step++)    
+    {
+        
+        path.apply_brownian();
+        path.update_path();
+        path.print_path();
+        visualizer.visualize(path.m_first_node, output_image);
+        cv::imshow("output", output_image);
+        cv::waitKey(10);
+    }
 
     return 0;
 }
