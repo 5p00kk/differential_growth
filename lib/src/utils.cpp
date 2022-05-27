cb19b7b4f8e0345f4596738730b2a26e64b747e9
5p00kk <szymon.kowalewski@gmail.com>
2022-05-27 17:38:33 +0200

    fix bug

--- lib/src/utils.cpp
+++ lib/src/utils.cpp
@@ -9,7 +9,7 @@ double dg::node_distance(const c_node& first, const c_node& second)
 dg::pt2 dg::nodes_midpoint(const c_node& first, const c_node& second)
 {
     pt2 midpoint{};
-    midpoint.x = std::min(first.curr_pos.x, second.curr_pos.x) + abs(first.curr_pos.x - second.curr_pos.x);
-    midpoint.y = std::min(first.curr_pos.y, second.curr_pos.y) + abs(first.curr_pos.y - second.curr_pos.y);
+    midpoint.x = std::min(first.curr_pos.x, second.curr_pos.x) + abs(first.curr_pos.x - second.curr_pos.x)/2;
+    midpoint.y = std::min(first.curr_pos.y, second.curr_pos.y) + abs(first.curr_pos.y - second.curr_pos.y)/2;
     return midpoint;
 }
